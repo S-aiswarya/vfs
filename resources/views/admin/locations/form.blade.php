945da94fc386b6ca44b81e0bbeefcd4c37bea114
S-aiswarya <saiswarya0626@gmail.com>
2024-08-14 11:43:26 +0530

    debug

--- resources/views/admin/locations/form.blade.php
+++ resources/views/admin/locations/form.blade.php
@@ -10,7 +10,7 @@
     <div class="row">
 
     <div class="form-group">
-            <label for="city">City</label>
+            <label for="city">Cities</label>
             <select name="city_id" style="width: 100% !important;" class="w-100 webadmin-select2-input form-control" data-parent=".confirm-wrap" data-select2-url="{{route('admin.select2.cities')}}">
               @if($obj->city)
                 <option value="{{$obj->city->id}}" selected="selected">{{$obj->city->name}}</option>
